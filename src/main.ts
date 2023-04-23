eea4da0ccf885aeae4c1bd90417095676e9ea598
jinhoe <jinhoe.kim@imweb.me>
2023-04-23 10:01:51 +0900

    feat: first swagger docs setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Whooing Everyday API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+
+  // config를 바탕으로 swagger document 생성
+  const document = SwaggerModule.createDocument(app, config);
+  // Swagger UI에 대한 path를 연결함
+  // .setup('swagger ui endpoint', app, swagger_document)
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
