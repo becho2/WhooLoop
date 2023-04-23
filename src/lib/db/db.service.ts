b5aadb635583d67485204541a8cf3c739ee0aaa8
jinhoe <jinhoe.kim@imweb.me>
2023-04-23 13:56:29 +0900

    feat: write dbService, add knex, mysql2

---
+++ src/lib/db/db.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import dbConfig from 'src/config/dbConfig';
+import knex, { Knex } from 'knex';
+
+@Injectable()
+export class DBService {
+  read: Knex;
+  write: Knex;
+  constructor(
+    @Inject(dbConfig.KEY) private config: ConfigType<typeof dbConfig>,
+  ) {
+    this.read = knex(this.config.mysql.readPoolOptions);
+    this.write = knex({
+      client: 'mysql2',
+      connection: this.config.mysql.writePoolOptions,
+      pool: {
+        min: 0,
+        max: process.env.NODE_ENV !== 'production' ? 1 : 10,
+      },
+      debug: process.env.NODE_ENV == 'development',
+    });
+  }
+}
