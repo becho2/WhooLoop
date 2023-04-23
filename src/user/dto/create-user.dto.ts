986feaeae2bec7a3cf84466987503b9845f2b97c
jinhoe <jinhoe.kim@imweb.me>
2023-04-23 13:11:31 +0900

    feat: write user dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,27 +1,7 @@
-import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { PickType } from '@nestjs/mapped-types';
+import { UserEntity } from '../entities/user.entity';
 
-export class CreateUserDto {
-  @ApiProperty({
-    type: 'string',
-    description: '이메일, 후잉 계정과 동일한 이메일 권장',
-    example: 'abcd1234@whooing.com',
-    required: true,
-  })
-  @Expose()
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-
-  @ApiProperty({
-    type: 'string',
-    description: '패스워드',
-    example: '2rjswjfEodkftbdjqt!?',
-    required: true,
-  })
-  @Expose()
-  @IsNotEmpty()
-  @IsString()
-  password: string;
-}
+export class CreateUserDto extends PickType(UserEntity, [
+  'email',
+  'password',
+]) {}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,51 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { IsDate, Length } from 'class-validator';
+
+export class UserEntity {
+  /** userIdx */
+  @ApiProperty({
+    type: 'number',
+    description: 'userIdx',
+    required: true,
+  })
+  @Expose()
+  user_idx: number;
+
+  /** email */
+  @ApiProperty({
+    type: 'string',
+    description: 'email',
+    required: true,
+  })
+  @Expose()
+  @Length(1, 50)
+  email: string;
+
+  /** password */
+  @ApiProperty({
+    type: 'string',
+    description: 'password',
+    required: true,
+  })
+  @Expose()
+  @Length(1, 255)
+  password: string;
+
+  /** isDeleted */
+  @ApiProperty({
+    type: 'string',
+    description: 'isDeleted',
+    example: 'N',
+    required: true,
+  })
+  @Expose()
+  @Length(1, 1)
+  is_deleted: string;
+
+  @IsDate()
+  created: string;
+
+  @IsDate()
+  updated_last: string;
+}
