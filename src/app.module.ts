14bac4b2877158b0dbfc211d98b79b0b5b261145
jinhoe <jinhoe.kim@imweb.me>
2023-04-23 12:46:11 +0900

    feat: nest g res user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WhooingEverydayModule } from './whooing-everyday/whooing-everyday.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [WhooingEverydayModule],
+  imports: [WhooingEverydayModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
