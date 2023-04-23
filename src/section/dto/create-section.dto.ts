fc549f4c28b6679e8853bca9c8756a56341cb529
jinhoe <jinhoe.kim@imweb.me>
2023-04-23 13:14:18 +0900

    fix: change auto imported package to swagger

--- src/section/dto/create-section.dto.ts
+++ src/section/dto/create-section.dto.ts
@@ -1,4 +1,4 @@
-import { PickType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/swagger';
 import { SectionEntity } from '../entities/section.entity';
 
 export class CreateSectionDto extends PickType(SectionEntity, [
--- src/trx/dto/create-trx.dto.ts
+++ src/trx/dto/create-trx.dto.ts
@@ -1,4 +1,4 @@
-import { PickType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/swagger';
 import { TrxEntity } from '../entities/trx.entity';
 
 export class CreateTrxDto extends PickType(TrxEntity, [
