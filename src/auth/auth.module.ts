0ae9db04048aaebfd6015664533ca9490a5517af
jinhoe <jinhoe.kim@imweb.me>
2023-05-01 18:20:57 +0900

    nest g co auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
-import { AuthController } from './auth.controller';
 import { UserService } from '../user/user.service';
-import { UserRepository } from 'src/user/user.repository';
 import { DBService } from 'src/lib/db/db.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UserRepository } from '../lib/repositories/user.repository';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
