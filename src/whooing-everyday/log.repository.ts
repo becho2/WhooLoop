f0eb2ee43067ab30f6ae50882151a23b2b2e6270
jinhoe <jinhoe.kim@imweb.me>
2023-05-15 21:34:08 +0900

    feat: logTable 추가

---
+++ src/whooing-everyday/log.repository.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { DBService } from '../lib/db/db.service';
+import { LogEntity } from './entities/log.entity';
+
+@Injectable()
+export class LogRepository {
+  logTable = 'log';
+
+  constructor(private readonly dbService: DBService) {}
+  async create(logEntity: LogEntity): Promise<any> {
+    return this.dbService.db.insert(logEntity).into(this.logTable);
+  }
+}
