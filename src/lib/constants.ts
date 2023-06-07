d773373a2ff4e2b7c636959eb599834906d8953d
jinhoe <jinhoe.kim@imweb.me>
2023-06-07 21:16:48 +0900

    feat: 후잉 section 정보 api 요청으로 가져오기

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+const WHOOING_API_BASE_URL = 'https://whooing.com/api/';
+export const API_SECTIONS_URL = WHOOING_API_BASE_URL + 'sections.json_array';
+
+export const WHOOING_WEBHOOK_BASE_URL = 'https://whooing.com/webhook/s/';
--- src/oauth/oauth.service.ts
+++ src/oauth/oauth.service.ts
@@ -10,6 +10,8 @@ import { CreateOauthUserDto } from './dto/create-oauth-user.dto';
 import { plainToInstance } from 'class-transformer';
 import { UpdateOauthUserDto } from './dto/update-oauth-user.dto';
 import { AuthService } from '../auth/auth.service';
+import * as crypto from 'crypto';
+import { API_SECTIONS_URL } from '../lib/constants';
 
 @Injectable()
 export class OauthService {
@@ -60,17 +62,82 @@ export class OauthService {
     const user = await this.findUserIdxByWhooingUserId(
       whooingAccessData.whooingUserId,
     );
+
+    // 후잉 user id가 DB에 없을 경우 insert, 이미 있을 경우 토큰만 update
     if (user === undefined) {
       userIdx = await this.createOauthUser(whooingAccessData);
+      if (userIdx) {
+        await this.afterCreateUser(userIdx, whooingAccessData);
+      }
     } else {
       this.updateOauthUser(user.user_idx, whooingAccessData);
       userIdx = user.user_idx;
+      await this.afterCreateUser(userIdx, whooingAccessData);
     }
 
     return this.authService.getAccessToken(
       whooingAccessData.whooingUserId,
       userIdx,
     );
+  }
+
+  /**
+   * 첫 로그인시 create user 후 처리
+   * 해당 user id에 존재하는 섹션리스트를 검색해서 DB에 넣어주기
+   * @param whooingAccessData
+   */
+  async afterCreateUser(
+    userIdx: number,
+    whooingAccessData: OauthAccessTokenResponseDto,
+  ) {
+    const sectionList = await this.resourceApiRequest(
+      API_SECTIONS_URL,
+      whooingAccessData,
+    );
+  }
+
+  async resourceApiRequest(
+    url: string,
+    whooingAccessData: OauthAccessTokenResponseDto,
+  ) {
+    const signature = crypto
+      .createHash('sha1')
+      .update(
+        `${process.env.WHOOING_APP_SECRET}|${whooingAccessData.whooingAccessTokenSecret}`,
+      )
+      .digest('hex');
+    const data = {
+      app_id: process.env.WHOOING_APP_ID,
+      token: whooingAccessData.whooingAccessToken,
+      signiture: signature,
+      nounce: `${+new Date()}-${Math.random()}`,
+      timestamp: +new Date() / 1000,
+    };
+
+    const xApiKey = Object.keys(data).map((k) => {
+      return `${k}=${data[k]}`;
+    });
+    const requestConfig = {
+      method: 'get',
+      url: url,
+      headers: {
+        'x-api-key': xApiKey.join(','),
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    };
+    return await axios
+      .request(requestConfig)
+      .then((response) => {
+        if (response.data.code !== 200) {
+          throw new BadRequestException(response.data.message);
+        }
+        console.log(response.data.results);
+        return response.data.results;
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new BadRequestException(error.message);
+      });
   }
 
   async getAccessData(
