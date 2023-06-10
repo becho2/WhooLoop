cc1399c8ad15fa132bcfcbbfea0a8b111efad957
jinhoe <jinhoe.kim@imweb.me>
2023-06-10 16:26:33 +0900

    feat: update time 기록을 위해 dto에 datetime 추가

--- src/account/dto/update-account.dto.ts
+++ src/account/dto/update-account.dto.ts
@@ -1,3 +1,7 @@
+import { IsDate } from 'class-validator';
 import { CreateAccountDto } from './create-account.dto';
 
-export class UpdateAccountDto extends CreateAccountDto {}
+export class UpdateAccountDto extends CreateAccountDto {
+  @IsDate()
+  updated_last = new Date();
+}
