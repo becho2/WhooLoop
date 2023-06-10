27f0d8c0cff403f2d3524586ae030dacb02fe25d
jinhoe <jinhoe.kim@imweb.me>
2023-06-10 09:50:40 +0900

    refactor: 미리 빌드하므로 yarn start 스크립트는 build제외 바로 start하도록 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     // for health check
-    return 'Hello! 후룹 서비스를 이용하시려면 여기로 이동해주세요. <a href="http://146.56.136.6:5173"></a>';
+    return 'Hello! 후룹 서비스를 이용하시려면 여기로 이동해주세요. <a href="http://146.56.136.6:5173">WhooLoop</a>';
   }
 }
