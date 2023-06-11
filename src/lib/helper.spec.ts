b0382bac2b9bc9a3db148d4af85df4534fe0db1a
jinhoe <jinhoe.kim@imweb.me>
2023-06-11 22:22:36 +0900

    test: 24시간제로 시간반환시 앞자리 2까지 가능한 점 반영

--- src/lib/helper.spec.ts
+++ src/lib/helper.spec.ts
@@ -5,7 +5,7 @@ describe('test getDateTimeNow helper functions', () => {
   it('YYYY-MM-DD HH:mm:ss format and starts with 20(until 2099 year true)', () => {
     expect(dateTimeNow).toMatch(
       new RegExp(
-        '20[0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-1][0-9]:[0-5][0-9]:[0-5][0-9]',
+        '20[0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]',
       ),
     );
   });
@@ -21,6 +21,6 @@ describe('test getToday helper functions', () => {
 describe('test getHHmm helper functions', () => {
   const HHmm: string = getHHmm();
   it('HHmm format', () => {
-    expect(HHmm).toMatch(new RegExp('[0-1][0-9][0-5][0-9]'));
+    expect(HHmm).toMatch(new RegExp('[0-2][0-9][0-5][0-9]'));
   });
 });
