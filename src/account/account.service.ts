5c92ae457f11c6e947319ee703773c91dd11ca66
jinhoe <jinhoe.kim@imweb.me>
2023-06-11 17:32:23 +0900

    feat: frequent_items URL 추가, 상수명 변경

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -5,7 +5,7 @@ import { AccountRepository } from './account.repository';
 import { WhooingResourceApiService } from '../lib/whooing-resource-api/whooing-resource-api.service';
 import { WhooingAccountResponseDto } from './dto/whooing-account-response.dto';
 import { OauthAccessTokenResponseDto } from '../oauth/dto/oauth-access-token-response.dto';
-import { API_ACCOUNTS_URL } from '../lib/constants';
+import { API_ACCOUNTS_ALL_URL } from '../lib/constants';
 import { OauthUserRepository } from '../oauth/oauth-user.repository';
 import { SectionRepository } from '../section/section.repository';
 import { plainToInstance } from 'class-transformer';
@@ -28,7 +28,7 @@ export class AccountService {
     whooingAccessData: OauthAccessTokenResponseDto,
   ): Promise<WhooingAccountResponseDto> {
     return await this.whooingApi.resourceApiRequest(
-      API_ACCOUNTS_URL + sectionId,
+      API_ACCOUNTS_ALL_URL + sectionId,
       whooingAccessData,
     );
   }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
 const WHOOING_API_BASE_URL = 'https://whooing.com/api/';
 export const API_SECTIONS_URL = WHOOING_API_BASE_URL + 'sections.json_array';
-export const API_ACCOUNTS_URL =
+export const API_ACCOUNTS_ALL_URL =
   WHOOING_API_BASE_URL + 'accounts.json_array?section_id=';
+
+export const API_FREQUENT_ITEMS_URL =
+  WHOOING_API_BASE_URL + 'frequent_items.json_array?section_id=';
 
 export const WHOOING_WEBHOOK_BASE_URL = 'https://whooing.com/webhook/s/';
