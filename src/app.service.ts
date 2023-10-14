313927600887873d445fd4332cbc7b65b9650285
jinhoe <jinhoe.kim@imweb.me>
2023-10-14 16:39:36 +0900

    chore: api서버 접속 홈 화면 정보 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     // for health check
-    return 'Hello! 후룹 서비스를 이용하시려면 여기로 이동해주세요. <a href="http://146.56.136.6:5173">WhooLoop</a>';
+    return `Hello! <br />
+      후룹 서비스를 이용하시려면 여기로 이동해주세요. <a href="https://whooloop.todakbank.org">WhooLoop</a> <br />
+      API 문서는 <a href="https://api.whooloop.todakbank.org/docs">여기</a>를 참고해주세요.`;
   }
 }
